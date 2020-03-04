4ab9a8a1513a360dc7c7fcd1f5d74c2a2522f4fa
salcedoe <esalcedo94@yahoo.com>
2020-03-04 10:08:35 -0800

    Heroku test

--- model/lax.js
+++ model/lax.js
@@ -1,14 +1,13 @@
 const mongoose = require('mongoose')
 
-const lax = new mongoose.Schema({
-  team1: String,
-  team2: String,
-  score1: Number,
-  score2: Number,
-  location: String
-
+const game = new mongoose.Schema({
+      team1: String,
+      team2: String,
+      score1: Number,
+      score2: Number,
+      location: String
 }, {timestamps: true})
 
-const Lax = mongoose.model('Lax', lax)
+const Game = mongoose.model('Game', game)
 
-module.exports = Lax
+module.exports = Game
